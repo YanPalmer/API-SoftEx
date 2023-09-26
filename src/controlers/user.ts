49cc037c582c29c90b62c86347780b3c70bb4631
Yan Palmer <86450167+YanPalmer@users.noreply.github.com>
2023-09-26 19:56:36 -0300

    A terminar

--- src/controlers/user.ts
+++ src/controlers/user.ts
@@ -17,9 +17,11 @@ export const cadastrarUsuario = async (req: Request, res: Response) => {
         user.name = req.body.name;
         user.email = req.body.email;
         user.password = req.body.password;
+        user.cpf = req.body.cpf;
+        // Salva no banco de dados o usuário
         await appDataSource.getRepository(User).save(user);
 
-        return res.status(201).json({ok: true});
+        return res.status(201).json({ok: true, message: user});
 
     } catch (error) {
         console.log(error, "Erro ao cadastrar usuário");
@@ -29,3 +31,22 @@ export const cadastrarUsuario = async (req: Request, res: Response) => {
 
     return res.status(200).json({ok: true}); 
 };
+
+
+/*
+interface Pessoa {
+    nome: string;
+    idade: number;
+    nacionalidade: string;
+    cpf: string;
+}
+
+const pessoa1: Pessoa = {
+    nome: "Yan",
+    idade: 20,
+    cpf: "123.456.789-10",
+    nacionalidade: ""
+}
+
+console.log(pessoa1);
+*/
